71cfff4e22443576793e5acaebc930d032af63e2
KDH1029 <dohappy1029@gmail.com>
2024-05-16 23:35:13 +0800

    Separate mongo db function into mongo.js file

--- main.js
+++ main.js
@@ -6,75 +6,60 @@ app.listen(port,()=>{
   console.log("port: "+port);
 });
 
-const mongoose = require('mongoose');
-const url = `mongodb+srv://KDH:4n3g7!WTY_zZVd-@cluster0.sthvvfu.mongodb.net/userDB?retryWrites=true&w=majority&appName=Cluster0`;
-const UserModel = require('./models/user');
+const db = require('./mongo')
 
-mongoose
-  .connect(url)
-  .then(()=>{
-    console.log("connected");
-  }).catch((err)=>{
-    console.log("error: ",err);
-  });
-
-app.get('/',(req,res)=>{
-  res.send("...");
-});
-app.get('/test/:params',(req,res)=>{
-  console.log(req.params);
-  console.log(req.query);
-});
-
-// data register
-app.get('/user/:id',(req,res)=>{
+// register data
+app.get('/create/:id',(req,res)=>{
   var userInfo = {
     id:req.params.id,
     pw:req.query.pw??"null"
   };
-  var newUser = new UserModel(userInfo);
-  newUser
-    .save()
+  db.create(userInfo)
     .then(data=>{
-      res.status(200).send(userInfo);
-    }).catch(err=>{
-      console.log(err);
-    });
-});
-
-// data inquiry
-app.get('/read',(req,res)=>{
-  UserModel
-    .find()
-    .then(data=>{
+      console.log(`db.create(${userInfo}): `,data);
       res.status(200).send(data);
     })
     .catch(err=>{
+      console.log("error: ",err);
       res.status(500).send(err);
     });
 });
-
+// search data
+app.get('/search',(req,res)=>{
+  db
+    .search(req.query.q)
+    .then(data=>{
+      console.log(`db.search(${req.query.q}): `,data);
+      res.status(200).send(data);
+    })
+    .catch(err=>{
+      console.log("error: ",err);
+      res.status(500).send(err);
+    });
+});
 // data update
-// /update?id="id"&new="new id"
 app.get('/update',(req,res)=>{
-  UserModel
-    .findOneAndUpdate({id:req.query.id},{id:req.query.new})
+  db
+    .update(JSON.parse(req.query.query),JSON.parse(req.query.obj))
     .then(data=>{
+      console.log(`db.update(${req.query.query},${req.query.obj}): `,data);
       res.status(200).send(data);
     })
     .catch(err=>{
+      console.log("error: ",err);
       res.status(500).send(err);
     });
 });
-
 // delete data
-app.get('/delete/:id',(req,res)=>{
-  UserModel
-    .remove({id:req.params.id})
+app.get('/delete/:query',(req,res)=>{
+  db
+    .del(JSON.parse(req.params.query))
     .then(data=>{
+      console.log(`db.del(${req.params.query}: `,data);
       res.status(200).send(data);
     })
     .catch(err=>{
+      console.log("error: ",err);
       res.status(500).send(err);
     });
 });
---
+++ mongo.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+const url = `mongodb+srv://KDH:4n3g7!WTY_zZVd-@cluster0.sthvvfu.mongodb.net/userDB?retryWrites=true&w=majority&appName=Cluster0`;
+const UserModel = require('./models/user');
+
+/**
+ * Connect mongo db
+ */
+mongoose
+  .connect(url)
+  .then(()=>{
+    console.log("connected");
+  }).catch((err)=>{
+    console.log("error: ",err);
+  });
+
+/**
+ * Register data into mongo db.
+ * @param {Object} obj object to create
+ * @returns create result
+ */
+function create(obj){
+  return new Promise((resolve,reject)=>{
+    UserModel
+      .create(obj)
+      .then(data=>resolve(data))
+      .catch(err=>reject(err));
+  });
+}
+
+/**
+ * Search data from mongo db.
+ * @param {Object} query search condition
+ * @returns search result
+ */
+function search(query){
+  return new Promise((resolve)=>{
+    UserModel
+      .find(query)
+      .then(data=>{
+        resolve(data);
+      })
+      .catch(err=>{
+        console.log(err);
+      });
+  });
+}
+
+/**
+ * Update existing data that match the query to given object.
+ * @param {Object} query search condition
+ * @param {Object} obj object to replace
+ * @returns update result
+ */
+function update(query,obj){
+  return new Promise((resolve)=>{
+    UserModel
+      .updateMany(query,obj)
+      .then(data=>{
+        resolve(data);
+      })
+      .catch(err=>{
+        console.log(err);
+      });
+  });
+}
+
+/**
+ * Delete object in mongo db
+ * @param {Object} query search condition
+ * @returns delete result
+ */
+function del(query){
+  return new Promise((resolve)=>{
+    UserModel
+      .deleteMany(query)
+      .then(data=>{
+        resolve(data);
+      })
+      .catch(err=>{
+        console.log(err);
+      });
+    });
+}
+
+module.exports = {create,find,update,del};
